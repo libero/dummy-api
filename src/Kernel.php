6a8d5c1a4076894d6e642c255a4dabe6bb4900f4
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-11-08 16:26:20 +0000

    Tweaks

--- src/Kernel.php
+++ src/Kernel.php
@@ -52,15 +52,12 @@ final class Kernel extends BaseKernel
         $loader->load("{$confDir}/{packages}/*".self::CONFIG_EXTS, 'glob');
         $loader->load("{$confDir}/{packages}/{$this->environment}/**/*".self::CONFIG_EXTS, 'glob');
         $loader->load("{$confDir}/{services}".self::CONFIG_EXTS, 'glob');
-        $loader->load("{$confDir}/{services}_{$this->environment}".self::CONFIG_EXTS, 'glob');
-        $loader->load("{$confDir}/{config}".self::CONFIG_EXTS, 'glob');
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes) : void
     {
         $confDir = "{$this->getProjectDir()}/config";
 
-        $routes->import("{$confDir}/{routes}/*".self::CONFIG_EXTS, '/', 'glob');
         $routes->import("{$confDir}/{routes}/{$this->environment}/**/*".self::CONFIG_EXTS, '/', 'glob');
         $routes->import("{$confDir}/{routes}".self::CONFIG_EXTS, '/', 'glob');
     }
