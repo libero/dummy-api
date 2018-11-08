ce98d1959737ab8839ebfbb023a5d549e1990e5a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-11-08 15:36:12 +0000

    Fix prod

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     Libero\ApiProblemBundle\ApiProblemBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true,],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
