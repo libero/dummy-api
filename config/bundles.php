273cf3a1d40e39448bf9f3e489b033aff0680f21
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-11-14 11:53:04 +0000

    Set up logging

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 return [
     Libero\ApiProblemBundle\ApiProblemBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
