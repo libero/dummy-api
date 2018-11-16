240ef024fa41f6114dd6c9936054913a57abb809
Chris Wilkinson <chriswilkinson84@gmail.com>
2018-11-16 12:53:05 +0000

    Set up logging (#6)
    * Set up logging
    
    * Newer option allows for more status codes
    
    * Make sure exceptions are logged in prod
    
    * Demonstrate a failure
    
    * Output logs
    
    * In the meantime
    
    * More messages
    
    * Remove ErrorController

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,7 @@ return [
     Libero\ApiProblemBundle\ApiProblemBundle::class => ['all' => true],
     Libero\ContentApiBundle\ContentApiBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
