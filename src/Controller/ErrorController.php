8d3bae2af263e940d0e47d706f861040e13631b1
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-11-16 09:31:22 +0000

    More messages

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -4,13 +4,27 @@ declare(strict_types=1);
 
 namespace Libero\DummyApi\Controller;
 
+use Psr\Log\LoggerInterface;
 use RuntimeException;
 use Symfony\Component\HttpFoundation\Response;
 
 final class ErrorController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function __invoke() : Response
     {
+        $this->logger->debug('Some debug message', ['some context' => 'foo']);
+
+        $this->logger->info('Some info message', ['some context' => 'bar']);
+
+        $this->logger->notice('Some notice message');
+
         throw new RuntimeException('Some exception', 0, new RuntimeException('Some other exception'));
     }
 }
