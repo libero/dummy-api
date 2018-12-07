cee7fb6cddeea6fa640d1b8ff8629cd4372ae61e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-12-07 15:26:26 +0000

    Enable format and locale negotiation

--- tests/ErrorTest.php
+++ tests/ErrorTest.php
@@ -30,4 +30,39 @@ final class ErrorTest extends KernelTestCase
 
         $this->assertXpathMatch('/problem:problem/problem:title', $dom, ['problem' => 'urn:ietf:rfc:7807']);
     }
+
+    /**
+     * @test
+     * @dataProvider languageProvider
+     * @backupGlobals true
+     */
+    public function it_negotiates_locale(string $acceptLanguage, string $expected) : void
+    {
+        $_SERVER['DEFAULT_LOCALE'] = 'de';
+        $_SERVER['POSSIBLE_LOCALES'] = 'en|es';
+
+        self::bootKernel();
+
+        $request = Request::create('/foo');
+        $request->headers->set('Accept-Language', $acceptLanguage);
+
+        $response = self::$kernel->handle($request);
+
+        $this->assertSame(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $this->assertSame('application/problem+xml; charset=utf-8', $response->headers->get('Content-Type'));
+        $this->assertSame($expected, $response->headers->get('Content-Language'));
+
+        $dom = FluentDOM::load($response->getContent());
+
+        $this->assertXpathMatch("/problem:problem[@xml:lang='${expected}']", $dom, ['problem' => 'urn:ietf:rfc:7807']);
+    }
+
+    public function languageProvider() : iterable
+    {
+        yield 'Any language' => ['*', 'en'];
+        yield 'English' => ['en', 'en'];
+        yield 'Spanish' => ['es', 'es'];
+        yield 'French' => ['fr', 'de'];
+        yield 'French or English' => ['fr, en;q=0.1', 'en'];
+    }
 }
